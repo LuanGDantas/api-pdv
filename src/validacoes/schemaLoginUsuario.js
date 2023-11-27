f8163db8ca089171dd4480a333a33c8f79ccf892
LuanGDantas <luan.dantas123@hotmail.com>
2023-11-26 22:49:11 -0300

    Criando joi schema do corpo da requisicao de login

---
+++ src/validacoes/schemaLoginUsuario.js
@@ -0,0 +1,15 @@
+const joi = require('joi');
+
+const schemaLoginUsuario = joi.object({
+    nome: joi.string().required().messages({
+        'any.required': 'O campo nome é obrigatório',
+        'string.empty': 'O campo nome é obrigatório',
+    }),
+    email: joi.string().email().required().messages({
+        'string.email': 'O campo email precisa ter um formato válido',
+        'any.required': 'O campo email é obrigatório',
+        'string.empty': 'O campo email é obrigatório',
+    }),
+});
+
+module.exports = schemaLoginUsuario;
