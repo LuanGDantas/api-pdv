2a351562b35ede711a43405ab35b255faf09abaf
LuanGDantas <luan.dantas123@hotmail.com>
2023-11-29 00:05:19 -0300

    Adicionando o cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const rotas = require('./rotas/rotas');
 
@@ -6,6 +7,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 app.use(rotas);
 
 app.listen(port);
