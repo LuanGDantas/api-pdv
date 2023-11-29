89a98283eb59aa172f656bd0f08123c844e4e187
LuanGDantas <luan.dantas123@hotmail.com>
2023-11-29 00:07:54 -0300

    Removendo autenticação da rota de lista categorias

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -1,20 +1,15 @@
 const { Router } = require('express');
 
 const cadastrarUsuario = require('../controladores/usuarios/cadastrarUsuario');
-
 const detalharUsuario = require('../controladores/usuarios/detalharUsuarios');
-
 const loginUsuario = require('../controladores/usuarios/loginUsuario');
+const listarCategorias = require('../controladores/usuarios/listarCategorias');
 
 const validarCorpoRequisicao = require('../intermediarios/validarCorpoRequisicao');
-
 const autenticacaoToken = require('../intermediarios/autenticacaoToken');
 
 const schemaUsuario = require('../validacoes/schemaUsuario');
-
 const schemaLoginUsuario = require('../validacoes/schemaLoginUsuario');
-
-const listarCategorias = require('../controladores/usuarios/listarCategorias');
 
 const rotas = Router();
 
@@ -22,10 +17,10 @@ rotas.post('/usuario', validarCorpoRequisicao(schemaUsuario), cadastrarUsuario);
 
 rotas.post('/login', validarCorpoRequisicao(schemaLoginUsuario), loginUsuario);
 
+rotas.get('/categoria', listarCategorias);
+
 rotas.use(autenticacaoToken);
 
 rotas.get('/usuario', detalharUsuario);
-
-rotas.get('/categoria', listarCategorias)
 
 module.exports = rotas;
