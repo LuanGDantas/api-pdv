1057c2c23a4328158601272c9d40bcbb14f13c9d
LuanGDantas <luan.dantas123@hotmail.com>
2023-11-30 22:53:24 -0300

    Criando rota Editar dados do Produto

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -5,9 +5,12 @@ const detalharUsuario = require('../controladores/usuarios/detalharUsuarios');
 const loginUsuario = require('../controladores/usuarios/loginUsuario');
 const atualizarUsuario = require('../controladores/usuarios/atualizarUsuario');
 const listarCategorias = require('../controladores/usuarios/listarCategorias');
+const atualizarProduto = require('../controladores/produtos/atualizarProduto');
 
 const validarCorpoRequisicao = require('../intermediarios/validarCorpoRequisicao');
 const autenticacaoToken = require('../intermediarios/autenticacaoToken');
+const verificarProdutoExiste = require('../intermediarios/verificarProdutoExiste');
+const verificarCategoriaExiste = require('../intermediarios/verificarCategoriaExiste');
 
 const schemaUsuario = require('../validacoes/schemaUsuario');
 const schemaLoginUsuario = require('../validacoes/schemaLoginUsuario');
@@ -25,5 +28,13 @@ rotas.use(autenticacaoToken);
 rotas.get('/usuario', detalharUsuario);
 
 rotas.put('/usuario', validarCorpoRequisicao(schemaUsuario), atualizarUsuario);
+
+rotas.put(
+    '/produto/:id',
+    validarCorpoRequisicao(schemaProduto),
+    verificarProdutoExiste,
+    verificarCategoriaExiste,
+    atualizarProduto,
+);
 
 module.exports = rotas;
