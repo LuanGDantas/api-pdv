b500793416adfad57231fba820f9c768df8a9035
LuanGDantas <luan.dantas123@hotmail.com>
2023-12-04 11:46:32 -0300

    Criando controlador Cadastrar Cliente

---
+++ src/controladores/clientes/cadastrarCliente.js
@@ -0,0 +1,44 @@
+const {
+    inserirCliente,
+    buscarClientePorEmail,
+    buscarClientePorCpf,
+} = require('../../repositorios/clientes');
+
+const cadastrarCliente = async (req, res) => {
+    const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } =
+        req.body;
+
+    try {
+        const clienteComMesmoEmailExiste = await buscarClientePorEmail(email);
+        if (!!clienteComMesmoEmailExiste) {
+            return res.status(400).json({
+                mensagem: 'Já existe cliente cadastrado com o e-mail informado',
+            });
+        }
+
+        const clienteComMesmoCpfExiste = await buscarClientePorCpf(email);
+        if (!!clienteComMesmoCpfExiste) {
+            return res.status(400).json({
+                mensagem: 'Já existe cliente cadastrado com o CPF informado',
+            });
+        }
+
+        const cliente = await inserirCliente(
+            nome,
+            email,
+            cpf,
+            cep,
+            rua,
+            numero,
+            bairro,
+            cidade,
+            estado,
+        );
+
+        return res.status(200).json(cliente);
+    } catch (error) {
+        return res.status(500).json({ mensage: 'Erro Interno no Servidor' });
+    }
+};
+
+module.exports = cadastrarCliente;
