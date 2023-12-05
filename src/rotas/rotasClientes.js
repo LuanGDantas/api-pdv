ead59f487afad4cc30dc9dd7e7ea4498947005dd
LuanGDantas <luan.dantas123@hotmail.com>
2023-12-05 10:01:51 -0300

    Ajustando importações

--- src/rotas/rotasClientes.js
+++ src/rotas/rotasClientes.js
@@ -1,13 +1,12 @@
 const { Router } = require('express');
-const autenticacaoToken = require('../../src/intermediarios/autenticacaoToken');
-const detalharCliente = require('../controladores/clientes/detalharCliente');
-const verificarClienteExiste = require('../intermediarios/verificarClienteExiste');
 
+const detalharCliente = require('../controladores/clientes/detalharCliente');
 const cadastrarCliente = require('../controladores/clientes/cadastrarCliente');
 const atualizarCliente = require('../controladores/clientes/atualizarCliente');
 const listarClientes = require('../controladores/clientes/listarClientes');
 
 const validarCorpoRequisicao = require('../intermediarios/validarCorpoRequisicao');
+const verificarClienteExiste = require('../intermediarios/verificarClienteExiste');
 const autenticacaoToken = require('../intermediarios/autenticacaoToken');
 
 const schemaCliente = require('../validacoes/schemaCliente');
