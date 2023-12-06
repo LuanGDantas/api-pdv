750721826afc7f755f8f39b6ba9ee1798523ea64
LuanGDantas <luan.dantas123@hotmail.com>
2023-12-06 19:06:23 -0300

    Ajustando validação de schema para campo não obrigatórios

--- src/validacoes/schemaCliente.js
+++ src/validacoes/schemaCliente.js
@@ -23,6 +23,20 @@ const schemaCliente = joi.object({
         'string.pattern.base':
             'O campo CPF deve conter apenas dígitos (Ex.: 12345678901)',
     }),
+
+    cep: joi.string().length(8).pattern(/^\d+$/).allow('', null).messages({
+        'string.base':
+            'O campo CEP deve ser uma string com os 8 dígitos (Ex.: 59900000)',
+        'string.length': 'O campo CEP deve ter 8 dígitos (Ex.: 59900000)',
+        'string.pattern.base':
+            'O campo CEP deve conter apenas dígitos (Ex.: 12345678901)',
+    }),
+
+    rua: joi.string().allow('', null),
+    numero: joi.string().allow('', null),
+    bairro: joi.string().allow('', null),
+    cidade: joi.string().allow('', null),
+    estado: joi.string().allow('', null),
 });
 
 module.exports = schemaCliente;
