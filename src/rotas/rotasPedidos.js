ca64cb2d0b409ab3b078099bf9aff317721a607a
LuanGDantas <luan.dantas123@hotmail.com>
2023-12-11 12:36:58 -0300

    Criando rota de Cadastrar Pedido

---
+++ src/rotas/rotasPedidos.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+
+const cadastarPedido = require('../controladores/pedidos/cadastrarPedido');
+
+const verificarClienteExiste = require('../intermediarios/verificarClienteExiste');
+
+const schemaPedido = require('../validacoes/schemaPedido');
+
+const rotasPedidos = Router();
+
+rotasPedidos.post(
+    '/pedido',
+    validarCorpoRequisicao(schemaPedido),
+    verificarClienteExiste,
+    cadastarPedido,
+);
+
+module.exports = rotasPedidos;
