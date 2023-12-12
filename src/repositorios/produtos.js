05e857a3b79f184202665502ea16f99080a1b3ec
Stefany Borin <stefanyceleste1730@gmail.com>
2023-12-12 12:53:29 -0300

    atualizando query de atualizar e cadastrar produto para o banco de dados enviar imagem produto

--- src/repositorios/produtos.js
+++ src/repositorios/produtos.js
@@ -5,6 +5,7 @@ const inserirProduto = async ({
     quantidade_estoque,
     valor,
     categoria_id,
+    produto_imagem,
 }) => {
     const [produto] = await bancoDeDados('produtos')
         .insert({
@@ -12,6 +13,7 @@ const inserirProduto = async ({
             quantidade_estoque,
             valor,
             categoria_id,
+            produto_imagem,
         })
         .returning('*');
 
@@ -24,7 +26,9 @@ const alterarProduto = async ({
     quantidade_estoque,
     valor,
     categoria_id,
+    produto_imagem,
 }) => {
+    console.log(produto_imagem);
     const [produto] = await bancoDeDados('produtos')
         .where({ id })
         .update({
@@ -32,6 +36,7 @@ const alterarProduto = async ({
             quantidade_estoque,
             valor,
             categoria_id,
+            produto_imagem,
         })
         .returning('*');
 
