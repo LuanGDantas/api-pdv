eaebc68e1ea04f1e643ff801d5a336e98e911d02
Stefany Borin <stefanyceleste1730@gmail.com>
2023-12-12 20:25:39 -0300

    Adicionando função delete upload de imagem

--- src/controladores/produtos/deletarProduto.js
+++ src/controladores/produtos/deletarProduto.js
@@ -1,9 +1,17 @@
 const { excluirProduto } = require('../../repositorios/produtos');
+const {deletarArquivo} = require('../../provedores/storage')
 
 const deletarProduto = async (req, res) => {
     const { id } = req.produto;
-
+    const produto = req.produto;
     try {
+        if (produto.produto_imagem) {
+            const path = produto.produto_imagem.replace(
+                `${process.env.URL_ENDPOINT}/${process.env.BLACKBLAZE_BUCKET}/`,
+                '',
+            );
+            await deletarArquivo(decodeURIComponent(path));
+        }
         const produtoExcluido = await excluirProduto(id);
         if (!produtoExcluido) {
             return res
