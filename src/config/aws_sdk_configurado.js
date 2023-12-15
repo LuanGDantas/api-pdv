c08a2b5fe8ff6a3f3bbf04dedc97622302c1364e
LuanGDantas <luan.dantas123@hotmail.com>
2023-12-15 11:45:24 -0300

    Renommeando variaceis de ambiente

--- src/config/aws_sdk_configurado.js
+++ src/config/aws_sdk_configurado.js
@@ -2,10 +2,10 @@ const { S3Client } = require('@aws-sdk/client-s3');
 
 const s3 = new S3Client({
     endpoint: process.env.URL_ENDPOINT,
-    region: process.env.AWS_REGION,
+    region: process.env.AWS_STOARGE_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.AWS_STOARGE_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_STORAGE_SECRET_ACCESS_KEY,
     },
 });
 
