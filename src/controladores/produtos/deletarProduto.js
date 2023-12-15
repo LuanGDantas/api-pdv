bc6fc0953a3249e4559d61dc005f99c8b119e738
Stefany Borin <stefanyceleste1730@gmail.com>
2023-12-14 21:04:21 -0300

    adicionando regra de negocio em controlador

--- src/controladores/produtos/deletarProduto.js
+++ src/controladores/produtos/deletarProduto.js
@@ -1,10 +1,18 @@
 const { excluirProduto } = require('../../repositorios/produtos');
-const {deletarArquivo} = require('../../provedores/storage')
+const { deletarArquivo } = require('../../provedores/storage');
+const { buscarPedidos } = require('../../repositorios/pedidos');
 
 const deletarProduto = async (req, res) => {
     const { id } = req.produto;
     const produto = req.produto;
+
     try {
+        const existeProduto = await buscarPedidos(id);
+        if (existeProduto) {
+            return res.status(400).json({
+                mensagem: `O produto só pode ser excluido se não estiver incluido a algum pedido`,
+            });
+        }
         if (produto.produto_imagem) {
             const path = produto.produto_imagem.replace(
                 `${process.env.URL_ENDPOINT}/${process.env.BLACKBLAZE_BUCKET}/`,
@@ -21,6 +29,7 @@ const deletarProduto = async (req, res) => {
 
         return res.status(200).json({ mensagem: `Sucesso ao deletar produto` });
     } catch (error) {
+        console.log(error.message);
         return res.status(500).json({ mensagem: 'Erro Interno do Servidor' });
     }
 };
